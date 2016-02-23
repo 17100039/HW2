f0edee8b7aa38e7f42dd50872a1b2b69661e8aee
a17100039 <17100039@lums.edu.pk>
2016-02-23 18:05:44 +0000

    First and second task complete.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,6 +12,34 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    # params[:sort_by]
+    @movies.order!(params[:sort_by])
+  
+  end
+
+  def updateComplete
+  end
+  
+  def updateCompletely
+    puts "In updateCompletely()"
+    @input = params[:movie]
+    puts @input
+    
+    
+    if @movie = Movie.find_by_title(@input[:name])
+      puts "Movie does exist"
+      if !(@input[:title]=="" && @input[:rating]=="" && @input[:release_date]=="")
+        @movie.update_attributes!(movie_params)
+        flash[:notice] = "#{@movie.title} was successfully updated."
+        redirect_to movies_path
+      else
+        flash[:notice] = "Movie not updated. Please fill in all the details."
+        redirect_to updateComplete_movies_path
+      end
+    else
+      flash[:notice] = "#{@input[:name]} does not exist"
+      redirect_to updateComplete_movies_path
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,14 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
-  resources :movies
+  resources :movies do
+    collection do
+      get 'updateComplete'
+      put 'updateCompletely'
+    end
+  end
+  
+  
   
   # Example resource route with options:
   #   resources :products do
